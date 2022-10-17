ab42daa088349ac9480640a7017108c6efc6db6d
snrtn <snrtn94@gmail.com>
2022-10-17 13:40:01 +0200

    save code

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,36 @@
-require('dotenv').config();
-require('express-async-errors');
+require("dotenv").config();
+require("express-async-errors");
 
 // middleware
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // express
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // database
-const connectDB = require('./db/connect');
+const connectDB = require("./db/connect");
+
+//  routers
+const authRouter = require("./routes/authRoutes");
+// const userRouter = require("./routes/userRoutes");
+// const productRouter = require("./routes/productRoutes");
+// const reviewRouter = require("./routes/reviewRoutes");
+// const orderRouter = require("./routes/orderRoutes");
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Page');
+app.get("/", (req, res) => {
+  res.send("Welcome to the Page");
 });
+
+app.use("/api/vl/auth", authRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
+
+// app.use();
 
 const port = process.env.PORT || 3000;
 const start = async () => {
