5f4e6a63ba692d5dfb0978599575f11649e05ff6
snrtn <snrtn94@gmail.com>
2023-01-10 10:38:07 +0100

    fix code

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,27 +1,45 @@
 // récupère notre model User ,créer avec le schéma mongoose
 const User = require('../models/userModels');
-const { StatusCodes } = require('http-status-codes');
+// const { StatusCodes } = require('http-status-codes');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 
-// sauvegarde un nouvel utilisateur
-const signup = async (req, res) => {      
-  const user = await User.create({ email: req.body.email, password: req.body.password });
-  user.save()
+exports.signup = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+      user.save()
+        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+        .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
 };
 
-const login = async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await User.findOne({ email: email, password: password });
-
-  const token = jwt.sign(
-    {expiresIn: '24h'},
-  )
-  res.status(StatusCodes.OK).json({ userId: user.id, token: token });
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+      .then(user => {
+          if (!user) {
+              return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+          }
+          bcrypt.compare(req.body.password, user.password)
+              .then(valid => {
+                  if (!valid) {
+                      return res.status(401).json({ error: 'Mot de passe incorrect !' });
+                  }
+                  res.status(200).json({
+                      userId: user._id,
+                      token: jwt.sign(
+                          { userId: user._id },
+                          'RANDOM_TOKEN_SECRET',
+                          { expiresIn: '24h' }
+                      )
+                  });
+              })
+              .catch(error => res.status(500).json({ error }));
+      })
+      .catch(error => res.status(500).json({ error }));
 };
-
-module.exports = {
-  login,
-  signup,
-}
